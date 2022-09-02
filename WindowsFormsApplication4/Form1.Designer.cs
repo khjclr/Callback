501bb30dabb1261068262fac583d4044cc60fbcd
khjclr <huajun_kuang@dgkind.com>
2022-09-02 16:45:26 +0800

    init

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnReg = new System.Windows.Forms.Button();
+            this.btnGetPara = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnCloseCamera = new System.Windows.Forms.Button();
+            this.btnLoop = new System.Windows.Forms.Button();
+            this.btnExitLoop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.hSmartWindowControl1 = new HalconDotNet.HWindowControl();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnReg
+            // 
+            this.btnReg.Location = new System.Drawing.Point(721, 485);
+            this.btnReg.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnReg.Name = "btnReg";
+            this.btnReg.Size = new System.Drawing.Size(128, 68);
+            this.btnReg.TabIndex = 1;
+            this.btnReg.Text = "注册回调";
+            this.btnReg.UseVisualStyleBackColor = true;
+            this.btnReg.Click += new System.EventHandler(this.btnReg_Click);
+            // 
+            // btnGetPara
+            // 
+            this.btnGetPara.Location = new System.Drawing.Point(1068, 485);
+            this.btnGetPara.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnGetPara.Name = "btnGetPara";
+            this.btnGetPara.Size = new System.Drawing.Size(128, 68);
+            this.btnGetPara.TabIndex = 2;
+            this.btnGetPara.Text = "获得参数";
+            this.btnGetPara.UseVisualStyleBackColor = true;
+            this.btnGetPara.Click += new System.EventHandler(this.btnGetPara_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(717, 42);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(473, 424);
+            this.listBox1.TabIndex = 3;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.保存ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(109, 28);
+            // 
+            // 保存ToolStripMenuItem
+            // 
+            this.保存ToolStripMenuItem.Name = "保存ToolStripMenuItem";
+            this.保存ToolStripMenuItem.Size = new System.Drawing.Size(108, 24);
+            this.保存ToolStripMenuItem.Text = "保存";
+            this.保存ToolStripMenuItem.Click += new System.EventHandler(this.保存ToolStripMenuItem_Click);
+            // 
+            // btnCloseCamera
+            // 
+            this.btnCloseCamera.Location = new System.Drawing.Point(1064, 732);
+            this.btnCloseCamera.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCloseCamera.Name = "btnCloseCamera";
+            this.btnCloseCamera.Size = new System.Drawing.Size(128, 68);
+            this.btnCloseCamera.TabIndex = 4;
+            this.btnCloseCamera.Text = "关闭相机";
+            this.btnCloseCamera.UseVisualStyleBackColor = true;
+            this.btnCloseCamera.Click += new System.EventHandler(this.btnCloseCamera_Click);
+            // 
+            // btnLoop
+            // 
+            this.btnLoop.Location = new System.Drawing.Point(721, 641);
+            this.btnLoop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnLoop.Name = "btnLoop";
+            this.btnLoop.Size = new System.Drawing.Size(128, 68);
+            this.btnLoop.TabIndex = 5;
+            this.btnLoop.Text = "循环开始";
+            this.btnLoop.UseVisualStyleBackColor = true;
+            this.btnLoop.Click += new System.EventHandler(this.btnLoop_Click);
+            // 
+            // btnExitLoop
+            // 
+            this.btnExitLoop.Location = new System.Drawing.Point(1064, 641);
+            this.btnExitLoop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnExitLoop.Name = "btnExitLoop";
+            this.btnExitLoop.Size = new System.Drawing.Size(128, 68);
+            this.btnExitLoop.TabIndex = 6;
+            this.btnExitLoop.Text = "循环结束";
+            this.btnExitLoop.UseVisualStyleBackColor = true;
+            this.btnExitLoop.Click += new System.EventHandler(this.btnExitLoop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(715, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "相机支持参数列表";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(719, 605);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(217, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "下面是循环方式取外触发的图像";
+            // 
+            // hSmartWindowControl1
+            // 
+            this.hSmartWindowControl1.BackColor = System.Drawing.Color.Black;
+            this.hSmartWindowControl1.BorderColor = System.Drawing.Color.Black;
+            this.hSmartWindowControl1.ImagePart = new System.Drawing.Rectangle(0, 0, 4096, 2168);
+            this.hSmartWindowControl1.Location = new System.Drawing.Point(11, 188);
+            this.hSmartWindowControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.hSmartWindowControl1.Name = "hSmartWindowControl1";
+            this.hSmartWindowControl1.Size = new System.Drawing.Size(698, 458);
+            this.hSmartWindowControl1.TabIndex = 72;
+            this.hSmartWindowControl1.WindowSize = new System.Drawing.Size(698, 458);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1229, 839);
+            this.Controls.Add(this.hSmartWindowControl1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExitLoop);
+            this.Controls.Add(this.btnLoop);
+            this.Controls.Add(this.btnCloseCamera);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnGetPara);
+            this.Controls.Add(this.btnReg);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "海康外触发测试";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReg;
+        private System.Windows.Forms.Button btnGetPara;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnCloseCamera;
+        private System.Windows.Forms.Button btnLoop;
+        private System.Windows.Forms.Button btnExitLoop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 保存ToolStripMenuItem;
+        private HalconDotNet.HWindowControl hSmartWindowControl1;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HalconDotNet;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public HTuple hv_AcqHandle = null;//定义相机句柄
+        public   HObject img = null;//定义图像变量
+        HalconDotNet.HalconAPI.HFramegrabberCallback delegateCallback;//定义一个委托
+        private void Form1_Load(object sender, EventArgs e)//窗体加载的时候委托绑定及开启相机等操作
+        {
+            //hSmartWindowControl1.MouseWheel += this.hSmartWindowControl1.HSmartWindowControl_MouseWheel;//注册控件滚轮事件,该控件双击自适应显示
+            try
+            {
+               
+                //给委托绑定
+                delegateCallback = MyCallbackFunction;
+                //开启相机
+                HOperatorSet.OpenFramegrabber("GigEVision2", 0, 0, 0, 0, 0, 0, "progressive",
+           -1, "default", -1, "false", "default", "A1", 0, -1, out hv_AcqHandle);//GigEVision2表示接口类型，Mv1表示相机名，不要用海康提供的第三方接口MVision（不支持回调)
+                HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "TriggerMode", "On");//硬件触发开
+                HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "AcquisitionMode", "SingleFrame");//采集模式，单帧模式
+                HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "TriggerSource", "Line1");//触发线为Line0
+                //HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "ExposureTime", 5000.0);//曝光时间
+                //HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "ExposureAuto", "Off");//关闭自动曝光
+                // HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "EventSelector", "ExposureEnd");//选择曝光结束事件有效---这里要和注册时的名字一致（如"EventExposureEnd")
+                HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "EventSelector", "Line1RisingEdge");//选择上升沿事件有效---这里要和注册时的名字一致（如"EventLine0RisingEdge")
+                HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "EventNotification", "On");//事件通知开关打开
+                //HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "grab_timeout", 5000);//采集超时时间---因为海康的暂时不支持EventExposureEnd，事件触发的时候图像还没有准备好，超时时间要设置较长
+                HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "grab_timeout", -1);//永不超时
+               // HOperatorSet.GrabImageStart(hv_AcqHandle, -1);//异步采集开启
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex.Message);//在输出命令符窗口输出错误消息
+                try
+                {
+                    HOperatorSet.CloseFramegrabber(hv_AcqHandle);
+                }
+                catch (Exception)
+                {
+
+                   
+                }
+               
+            }
+         
+        }
+        private int test = 1;//随便定义的一个变量，后面会取其地址带入回调函数的user_context
+       
+        public  int MyCallbackFunction(IntPtr handle, IntPtr context, IntPtr user_context)//回调函数
+        {
+            try
+            {
+               // HOperatorSet.GrabImageAsync(out img, hv_AcqHandle, -1);//异步采集一帧
+                HOperatorSet.GrabImage(out img, hv_AcqHandle);//这里是同步采集一帧
+                if (this.hSmartWindowControl1.InvokeRequired)//线程亲和性判定
+                {
+                    this.Invoke(new MethodInvoker(() => { HOperatorSet.DispObj(img, this.hSmartWindowControl1.HalconWindow); img.Dispose(); }));//把图像显示出来（这里是委托方式显示)
+                }
+                else
+                {
+                    HOperatorSet.DispObj(img, this.hSmartWindowControl1.HalconWindow);//把图像显示出来
+                    img.Dispose();
+                }
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex.Message);//在输出命令符窗口输出错误消息
+                return -1;
+            }
+          
+        }
+        private void btnReg_Click(object sender, EventArgs e)//这里注册回调
+        {
+            try
+            {
+                IntPtr ptr = Marshal.GetFunctionPointerForDelegate(delegateCallback);//取回调函数的地址
+                IntPtr ptr1 = GCHandle.Alloc(test, GCHandleType.Pinned).AddrOfPinnedObject();//取test变量的地址
+                //Marshal.GetDelegateForFunctionPointer(ptr, typeof(HalconDotNet.HalconAPI.HFramegrabberCallback));
+                //因为Basler的相机不支持"transfer_end"等模式，查询了下支持 "ExposureEnd"作演示
+                //EventExposureEnd事件海康的需要调用HOperatorSet.GrabImage后才能触发，不满足要求，所以这里选择EventLine0RisingEdge测试，后续也许海康会升级Fw对应
+                //异步采集支持EventExposureEnd
+                //HOperatorSet.SetFramegrabberCallback(hv_AcqHandle, "EventExposureEnd", ptr, ptr1);//注册回调函数--这里与前面的"EventSelector"对应的要多Event前缀，海康的这样的
+                HOperatorSet.SetFramegrabberCallback(hv_AcqHandle, "EventLine1RisingEdge", ptr, ptr1);//注册回调函数--这里与前面的"EventSelector"对应的要多Event前缀，海康的这样的
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex.Message);//在输出命令符窗口输出错误消息
+                try
+                {
+                    HOperatorSet.CloseFramegrabber(hv_AcqHandle);
+                }
+                catch (Exception)
+                {
+
+
+                }
+            }
+        }
+        private void btnGetPara_Click(object sender, EventArgs e)//返回支持available_callback_types的列表参数
+        {
+            try
+            {
+                HTuple Value = null;
+                //下面返回支持回调的列表
+                HOperatorSet.GetFramegrabberParam(hv_AcqHandle, "available_callback_types", out Value);
+                int l = Value.Length;
+                for (long i = 0; i < l; i++)
+                {
+                    this.listBox1.Items.Add(Value[i].S);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Trace.WriteLine(ex.Message);//在输出命令符窗口输出错误消息
+            }
+          
+         }
+        private void btnCloseCamera_Click(object sender, EventArgs e)//关闭相机
+        {
+            if (hv_AcqHandle == null) return;
+            HOperatorSet.CloseFramegrabber(hv_AcqHandle);
+        }
+        public static void Delay(double t)//精确延时
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Restart();
+            while (stopWatch.Elapsed.TotalMilliseconds < t)
+            {
+                Application.DoEvents();
+            }
+        }
+        //-------以下是循环方式采集图像--------------------------------------
+        private bool exitloop = false;//推出循环命令
+        private void btnLoop_Click(object sender, EventArgs e)//采用循环的方式去取图
+        {
+            exitloop = false;
+            btnExitLoop.Focus();
+            Delay(50);
+            while(exitloop==false)
+            {
+                Application.DoEvents();
+                try
+                {
+                      HOperatorSet.GrabImage(out img, hv_AcqHandle);
+                      HOperatorSet.DispObj(img, this.hSmartWindowControl1.HalconWindow);
+                      Delay(50);
+                   
+                }
+                catch (Exception)
+                {
+                    Delay(50);
+                    continue;
+                }
+            }
+            MessageBox.Show("退出了");
+        }
+
+        private void btnExitLoop_Click(object sender, EventArgs e)//退出循环
+        {
+            exitloop = true;
+        }
+        private void hSmartWindowControl1_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+        private void 保存ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string str = "";
+            for(int i=0;i<listBox1.Items.Count;i++)
+            {
+                str += listBox1.Items[i] + "\r\n";
+            }
+            System.IO.File.WriteAllText("D:\\test\\txt.txt", str);
+        }
+    }
+}
